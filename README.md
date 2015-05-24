e40cc9a0686941800a18ab585f549cb076c29735
Eugene Sharygin <eush77@gmail.com>
2015-05-24 14:30:53 +0300

    Parse options with minimist

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Dual syntax highlighter for `--print-code` / `--print-opt-code` V8 dumps. Highli
 ## CLI
 
 ```
-v8-print-code-highlighter [file]
+Usage:  v8-print-code-highlighter [file]
 ```
 
 Reads standard input by default, you can pass filename to make it read from file.
--- cli.js
+++ cli.js
@@ -6,7 +6,9 @@ var highlightSection = require('./src/highlight-section');
 var codeDumpParser = require('v8-code-dump-parser'),
     helpVersion = require('help-version')(usage()),
     concat = require('parse-concat-stream'),
-    normalizeCss = require('normalize-css/normalize');
+    normalizeCss = require('normalize-css/normalize'),
+    minimist = require('minimist'),
+    camelCaseKeys = require('camelcase-keys');
 
 var fs = require('fs');
 
@@ -16,12 +18,23 @@ function usage() {
 }
 
 
-(function main(argv) {
-  if (argv.length > 1) {
+var options = camelCaseKeys(minimist(process.argv.slice(2), {
+  unknown: function (option) {
+    if (option[0] == '-') {
+      console.error('Unknown option: ' + option);
+      console.error();
+      helpVersion.help(1);
+    }
+  }
+}));
+
+
+(function main() {
+  if (options._.length > 1) {
     helpVersion.help(1);
   }
 
-  var input = argv.length ? fs.createReadStream(argv[0]) : process.stdin;
+  var input = options._.length ? fs.createReadStream(options._[0]) : process.stdin;
 
   input.pipe(concat({ parse: codeDumpParser }, function (err, sections) {
     if (err) throw err;
@@ -39,4 +52,4 @@ function usage() {
       '</pre>'
     ].join(''));
   }));
-}(process.argv.slice(2)));
+}());
